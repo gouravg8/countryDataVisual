008e723a752d0876f9d34da8368b46bf0eb25a3f
Gourav soni <gouravsoni8476@gmail.com>
2023-09-12 21:43:57 +0530

    added sorting functionalities by name, capName, pop

--- dist/completeCountriesInfo.js
+++ dist/completeCountriesInfo.js
@@ -9,40 +9,144 @@ let divCountriesArea = document.querySelector(".countriesArea");
 let txtCountResult = document.querySelector(".countResult");
 
 let oneCountry = (country) => {
-  return `<div
+  let out = `<div
     class="oneCountry p-3 mx-auto w-4/5 bg-white flex flex-col justify-center align-middle text-center md:mt-0 md:w-64 md:mx-0">
     <img class="" src="${country.flag}" alt="" srcset="">
         <p class="countryName text-orange-400 text-2xl font-semibold md:text-xl lg:text-2xl">${country.name}</p>
         <p class="text-left text-lg font-semibold md:text-base">Capital: <span
                 class="font-normal capitalAns">${country.capital}</span></p>
         <p class="text-left text-lg font-semibold md:text-base">Languages: <span
-                class="font-normal languagesAns capitalAns">${country.languages}</span></p>
+                class="font-normal languagesAns">${country.languages}</span></p>
         <p class="text-left text-lg font-semibold md:text-base">Populations: <span
-                class="font-normal populationAns capitalAns">${country.population}</span></p>
+                class="font-normal populationAns">${country.population}</span></p>
 </div>`;
+  return out;
 };
 
-inpSearch.addEventListener("input", (e) => {
+// crete country
+let createCountry = (searchString) => {
   let count = 0;
-  let firstCharOfInpVal = e.target.value.charAt(0).toUpperCase();
-  let inpVal = e.target.value.slice(1);
-  let searchString = firstCharOfInpVal + inpVal;
   divCountriesArea.textContent = "";
 
+  // orderChange(searchString);
+
   countries_data.forEach((country) => {
-    // if (country.name.includes(searchString)) {
-    //   console.log(country);
-    // }
-    country.languages.forEach((lang) => lang);
     if (
-      country.name.includes(searchString) ||
-      (country.capital && country.capital.includes(searchString)) ||
-      country.languages.find((lang) => lang.includes(searchString))
+      searchString &&
+      (country.name.includes(searchString) ||
+        (country.capital && country.capital.includes(searchString)) ||
+        country.languages.find((lang) => lang.includes(searchString)))
     ) {
       divCountriesArea.innerHTML += oneCountry(country);
       count++;
     }
-});
-txtCountResult.style.display = "flex";
-txtCountResult.textContent = count;
+    txtCountResult.textContent =
+      count == 1 || count == 0
+        ? count + " country satisfy search criteria"
+        : count + " countries satisfy search criteria";
+    txtCountResult.style.display = "block";
+  });
+};
+
+//* Function to reverse the order of divs by name
+let statusName = 0;
+function reverseDivsByName() {
+  const divs = Array.from(divCountriesArea.querySelectorAll(".oneCountry"));
+  if (!statusName) {
+    statusName = 1;
+    divs.sort((a, b) => {
+      const nameA = a.querySelector(".countryName").textContent;
+      const nameB = b.querySelector(".countryName").textContent;
+      btnName.textContent = 'Name ' + '↑'
+      return nameB.localeCompare(nameA);
+    });
+  } else {
+    statusName = 0;
+    divs.sort((a, b) => {
+      const nameA = a.querySelector(".countryName").textContent;
+      const nameB = b.querySelector(".countryName").textContent;
+      btnName.textContent = 'Name ' + '↓'
+      return nameA.localeCompare(nameB);
+    });
+  }
+  // Clear the divCountriesArea
+  divCountriesArea.innerHTML = "";
+  // Append the sorted divs back to the divCountriesArea
+  divs.forEach((div) => {
+    divCountriesArea.appendChild(div);
+  });
+}
+
+//* Add a click event listener to the button
+btnName.addEventListener("click", reverseDivsByName);
+
+//* Function to reverse the order of divs by Capitals name
+let statusCapital = 0;
+function reverseDivsByCapitalName() {
+  const divs = Array.from(divCountriesArea.querySelectorAll(".oneCountry"));
+  if (statusCapital) {
+    statusCapital = 0;
+    divs.sort((a, b) => {
+      const capA = a.querySelector(".capitalAns").textContent;
+      const capB = b.querySelector(".capitalAns").textContent;
+      btnCapital.textContent = 'Capital ' + '↑'
+      return capB.localeCompare(capA);
+    });
+  } else {
+    statusCapital = 1;
+    divs.sort((a, b) => {
+      const capA = a.querySelector(".capitalAns").textContent;
+      const capB = b.querySelector(".capitalAns").textContent;
+      btnCapital.textContent = 'Capital ' + '↓'
+      return capA.localeCompare(capB);
+    });
+  }
+  // Clear the divCountriesArea
+  divCountriesArea.innerHTML = "";
+  // Append the sorted divs back to the divCountriesArea
+  divs.forEach((div) => {
+    divCountriesArea.appendChild(div);
+  });
+}
+
+//* Add a click event listener to the button
+btnCapital.addEventListener("click", reverseDivsByCapitalName);
+
+// Function to reverse the order of divs by Capitals name
+let statusPopulation = 0;
+function reverseDivsByPopu() {
+  const divs = Array.from(divCountriesArea.querySelectorAll(".oneCountry"));
+  if (statusPopulation) {
+    statusPopulation = 0;
+    divs.sort((a, b) => {
+      const popA = +a.querySelector(".populationAns").textContent;
+      const popB = +b.querySelector(".populationAns").textContent;
+      btnPopulation.textContent = 'Population ' + '↑'
+      return popB - popA;
+    });
+  } else {
+    statusPopulation = 1;
+    divs.sort((a, b) => {
+      const popA = +a.querySelector(".populationAns").textContent;
+      const popB = +b.querySelector(".populationAns").textContent;
+      btnPopulation.textContent = 'Population ' + '↓'
+      return popA - popB;
+    });
+  }
+  // Clear the divCountriesArea
+  divCountriesArea.innerHTML = "";
+  // Append the sorted divs back to the divCountriesArea
+  divs.forEach((div) => {
+    divCountriesArea.appendChild(div);
+  });
+}
+
+//* Add a click event listener to the button
+btnPopulation.addEventListener("click", reverseDivsByPopu);
+
+inpSearch.addEventListener("input", (e) => {
+  let firstCharOfInpVal = e.target.value.charAt(0).toUpperCase();
+  let inpVal = e.target.value.slice(1);
+  let searchString = (firstCharOfInpVal + inpVal).trim();
+  createCountry(searchString);
 });
