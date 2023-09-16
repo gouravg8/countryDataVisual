7bb44f120be84c671f53d64e795855917e8c6d6a
Gourav Soni <84095227+gouravg8@users.noreply.github.com>
2023-09-16 19:30:49 +0530

    Update index.mjs for paths for github

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,10 +1,19 @@
 const index = [
-  { name: "Index", url: "../index.html" },
-  { name: "Country Data", url: "country.html" },
-  { name: "Portfolio", url: "portfolio.html" },
-  { name: "LeaderBoard", url: "leaderboard.html" },
-  { name: "NewAnimaiton", url: "challengeAnimation.html" },
-  { name: "ComConInfo", url: "completeCountriesInfo.html" },
+  // for development
+  // { name: "Index", url: "../index.html" },
+  // { name: "Country Data", url: "country.html" },
+  // { name: "Portfolio", url: "portfolio.html" },
+  // { name: "LeaderBoard", url: "leaderboard.html" },
+  // { name: "NewAnimaiton", url: "challengeAnimation.html" },
+  // { name: "ComConInfo", url: "completeCountriesInfo.html" },
+
+  // for github pages
+  { name: "Index", url: "./index.html" },
+  { name: "Country Data", url: "./dist/country.html" },
+  { name: "Portfolio", url: "./dist/portfolio.html" },
+  { name: "LeaderBoard", url: "./dist/leaderboard.html" },
+  { name: "NewAnimaiton", url: "./dist/challengeAnimation.html" },
+  { name: "ComConInfo", url: "./dist/completeCountriesInfo.html" },
 ];
 
 export default index;
