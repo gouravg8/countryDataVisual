45774f804916078847533a14842b645eb08da663
Gourav soni <gouravsoni8476@gmail.com>
2023-09-16 19:23:19 +0530

    added relative paths to the files

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,5 +1,5 @@
 const index = [
-  { name: "Index", url: "index.html" },
+  { name: "Index", url: "../index.html" },
   { name: "Country Data", url: "country.html" },
   { name: "Portfolio", url: "portfolio.html" },
   { name: "LeaderBoard", url: "leaderboard.html" },
